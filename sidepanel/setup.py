47cc6a9bb2fd9e7fb83e3d14c6e46d90a104625b
Ben Breedlove <benbreedlove@gmail.com>
2013-03-25 01:11:17 -0700

    setup file for heroku

---
+++ sidepanel/setup.py
@@ -0,0 +1,16 @@
+from distutils.core import setup
+
+setup(
+        name='sidepanel',
+        version='0.0.1',
+        author='ben breedlove',
+        author_email='benbreedlove@gmail.com',
+        packages=[],
+        scripts=[],
+        url='',
+        license='',
+        description='user modifyable sidepanels for mezzanine',
+        # long_description=open('').read(),
+        install_requires=[
+            ],
+        )
